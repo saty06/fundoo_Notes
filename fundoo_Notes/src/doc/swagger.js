5e964bbe607b51da479ca6f8cf0e1fcbaff8ecb9
saty06 <saty06072000@gmail.com>
2024-08-16 16:05:32 +0530

    [satya]addes:multer file

--- fundoo_Notes/src/doc/swagger.js
+++ fundoo_Notes/src/doc/swagger.js
@@ -1,44 +1,44 @@
-// src/doc/swagger.js
-import swaggerAutogen from 'swagger-autogen';
+// // src/doc/swagger.js
+// //import swaggerAutogen from 'swagger-autogen';
 
-const doc = {
-  openapi: '3.1.0',
-  info: {
-    title: 'My API',
-    description: 'Description',
-    version: '1.0.0'
-  },
-  servers: [
-    {
-      url: 'http://localhost:3000/api/v1/',
-      description: 'Local server'
-    }
-  ],
-  tags: [
-    {
-      name: 'User',
-      description: 'Operations about user'
-    },
-    {
-      name: 'Notes',
-      description: 'Operations about notes'
-    },
-    {
-      name: 'Default',
-      description: 'Welcome to the application'
-    }
-  ],
-  paths: {}
-};
+// const doc = {
+//   openapi: '3.1.0',
+//   info: {
+//     title: 'My API',
+//     description: 'Description',
+//     version: '1.0.0'
+//   },
+//   servers: [
+//     {
+//       url: 'http://localhost:3000/api/v1/',
+//       description: 'Local server'
+//     }
+//   ],
+//   tags: [
+//     {
+//       name: 'User',
+//       description: 'Operations about user'
+//     },
+//     {
+//       name: 'Notes',
+//       description: 'Operations about notes'
+//     },
+//     {
+//       name: 'Default',
+//       description: 'Welcome to the application'
+//     }
+//   ],
+//   paths: {}
+// };
 
-const outputFile = './src/doc/swagger-output.json';
-const endpointsFiles = [
-  '../index.ts',
-  '../routes/index.ts',
-  '../routes/user.route.ts',
-  '../routes/notes.routes.ts'
-];
+// const outputFile = './src/doc/swagger-output.json';
+// const endpointsFiles = [
+//   '../index.ts',
+//   '../routes/index.ts',
+//   '../routes/user.route.ts',
+//   '../routes/notes.routes.ts'
+// ];
 
-swaggerAutogen()(outputFile, endpointsFiles, doc).then(() =>{
-  console.log('Swagger documentation generated successfully');
-});
+// swaggerAutogen()(outputFile, endpointsFiles, doc).then(() =>{
+//   console.log('Swagger documentation generated successfully');
+// });
